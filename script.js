5b4775d2428776ec18b09b67908bbc9477c65bb5
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-11-18 22:45:07 -0500

    EOD. Trying to get return to home page to work.

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
 const main = document.querySelector('div.main')
-// console.log('div.main', main)
 const container = document.querySelector('.container')
+const homeLink = document.getElementById("home")
+const portfolioLink = document.getElementById("portfolio")
+const contactLink = document.getElementById("contact")// append contact info
 
 // on click div class container is removed
 function emptyContainer (cont) {
   cont.remove()
 }
-//generate new container
+
 function generateContainer () {
   const divCont = document.createElement("div");
   divCont.className = 'container'
+
   return divCont;
 }
 
-//add eventListener to Resume
-const portfolioLink = document.getElementById("portfolio")
-// append portfolio
-portfolioLink.addEventListener('click', e => {
-  console.log(`Portfolio link clicked`, e)
-  emptyContainer(container)
-  const divContainer = generateContainer()
-  //new Image()
+function generatePortfolio () {
   const portfolio = new Image()
   portfolio.src = './images/generic-resume.jpeg'
   portfolio.alt = 'portfolio doc'
   portfolio.setAttribute('id', 'portfolio') 
+  
+  return portfolio
+}
 
+function appendResume (cont) {
+  emptyContainer(cont)
+  const divContainer = generateContainer()
+  const portfolio = generatePortfolio()
+  
   divContainer.append(portfolio)
-  console.log('portfolio', portfolio)
   main.append(divContainer)
+}
+
+function generateHeadshot () {
+  const headshot = new Image()
+  headshot.src = './images/headshot.jpeg'
+  headshot.alt = 'Profile Picture'
+  headshot.setAttribute('id', 'headshot')
+
+  return headshot
+}
+
+function generateBio() {
+  const bio = document.createElement('p')
+  bio.className = 'bio'
+  bio.innerText = 'Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.'
+  return bio
+}
+
+portfolioLink.addEventListener('click', e => {
+  appendResume(container)
 })
 
-
-const contactLink = document.getElementById("contact")// append contact info
 contactLink.addEventListener('click', e => {
   console.log(`Contact link clicked`, e)
 })
 
-const homeLink = document.getElementById("home")
 homeLink.addEventListener('click', e => {
-  console.log(`Home link clicked`, e)
+  console.log('container', container)
+  emptyContainer(container)
+
+  const divContainer = generateContainer()
+  const headshot = generateHeadshot()
+  const bio = generateBio()
+  // console.log('headshot', headshot)
+  // headshot.append(bio)
+  // main.append(headshot)
 })
-// append homepage
+
